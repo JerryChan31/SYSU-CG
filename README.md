6e87174bf12a125c64059ab8a3a6542bc7b4b84d
JerryChan31 <331498453@qq.com>
2018-04-17 00:49:03 +0800

    homework 6 finished.(include bonus)

--- README.md
+++ README.md
@@ -2,5 +2,13 @@
 中山大学2018年计算机图形学课程作业 by 陈康怡
 
 Progress: 
-hw1 finished. (include bonus)
+
 hw2 finished. (include bonus)
+
+hw3 finished. (include bonus)
+
+hw4 finished. 
+
+hw5 finished. 
+
+hw6 finished. (include bonus)
--- hw5/hw5/src1.cpp
+++ hw5/hw5/src1.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
-glm::vec3 lightPosition(2.0f, 1.0f, 2.0f);
+
 
 int main() {
   glfwInit();
@@ -46,67 +46,91 @@ int main() {
   ImGui::StyleColorsDark();
 
   Shader shader("vShaderSrc.txt", "fShaderSrc.txt");
+  Shader Gshader("GouraudV.txt", "GouraudF.txt");
   Shader lightShader("vShaderSrc.txt", "lightfShaderSrc.txt");
   // Triangle info
   ImVec4 tri1 = ImVec4(1.0f, 0.0f, 0.0f, 1.00f);
   ImVec4 tri2 = ImVec4(0.0f, 1.0f, 0.0f, 1.00f);
   ImVec4 tri3 = ImVec4(0.0f, 0.0f, 1.0f, 1.00f);
   ImVec4 tri4 = ImVec4(0.5f, 0.f, 0.5f, 1.00f);
+
   float vertices[] = {
-    -0.2f, -0.2f, -0.2f,
-    -0.2f, -0.2f,  0.2f, 
-    -0.2f,  0.2f, -0.2f, 
-    -0.2f,  0.2f,  0.2f, 
-     0.2f, -0.2f, -0.2f, 
-     0.2f, -0.2f,  0.2f, 
-     0.2f,  0.2f, -0.2f, 
-     0.2f,  0.2f,  0.2f
-  };
-  unsigned int triangleIndices[] = {
-    0, 1, 3, 
-    0, 3, 2,
-    0, 1, 5, 
-    0, 5, 4,
-    3, 2, 7,
-    2, 7, 6,
-    3, 1, 5,
-    3, 5, 7,
-    0, 2, 6,
-    0, 4, 6,
-    5, 4, 7,
-    4, 7, 6
+    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+    -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+    0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+    0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+    0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+    0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+    0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+    0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+    0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+    0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+    0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+    0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
   };
 
-  unsigned int cubeVAO, VBO, EBO, lightVAO;
+  unsigned int cubeVAO, VBO, lightVAO;
   // cube VAO&VBO
   glGenVertexArrays(1, &cubeVAO);
   glGenBuffers(1, &VBO);
   glBindVertexArray(cubeVAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-  glGenBuffers(1, &EBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangleIndices), triangleIndices, GL_STATIC_DRAW);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+  glEnableVertexAttribArray(1);
 
   // light VAO &VBO
   glGenVertexArrays(1, &lightVAO);
   glBindVertexArray(lightVAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glGenBuffers(1, &EBO);
-  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(triangleIndices), triangleIndices, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
   
 
-  glm::mat4 view;
+  
   bool depth = true;
   int radioMark = 0;
   float temp = 0.0f;
   float save = 0.0f;
   int sign = 1;
+  bool stop = false;
+  glm::vec3 lightPosition(-25.0f, 7.0f, -22.0f);
+  glm::vec3 viewPosition = glm::vec3(6.0f, 2.0f, 5.0f);
+  float ambientStrength = 0.3;
+  float specularStrength = 0.9;
+  float concentrate = 3200;
+  float diffuseMutiple = 0.8;
   // ---- render loop ----
   while (!glfwWindowShouldClose(window)) {
     // process input from keyboard/mouse/other
@@ -114,54 +138,73 @@ int main() {
     glfwPollEvents();
     // init ImGui
     ImGui_ImplGlfwGL3_NewFrame();
-
+    ImGui::RadioButton("Phong", &radioMark, 0);
+    ImGui::RadioButton("Gouraud", &radioMark, 1);
+    ImGui::RadioButton("Bonus", &radioMark, 2);
+    ImGui::DragFloat("Ambient", &ambientStrength, 0.01f, 0.0f, 1.0f);
+    ImGui::DragFloat("Diffuse", &diffuseMutiple, 0.01f, 0.0f, 1.0f);
+    ImGui::DragFloat("Specular", &specularStrength, 0.01f, 0.0f, 3.0f);
+    ImGui::DragFloat("Shininess", &concentrate, 10.0f, 32.0f, 3200.0f);
+    ImGui::DragFloat3("LIGHT POS", &lightPosition.x, 2, 0);
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glClear(GL_DEPTH_BUFFER_BIT);
     glEnable(GL_DEPTH_TEST);
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 projection;
+    view = glm::lookAt(viewPosition, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+    projection = glm::perspective(20.0f, 1.0f, 0.01f, 100.0f);
+    float radius = 20.0f;
+    float camX = sin(glfwGetTime()) * radius;
+    float camZ = cos(glfwGetTime()) * radius;
+    if (radioMark == 0 || radioMark == 2) {
+      if (radioMark == 2) lightPosition = glm::vec3(camX, lightPosition.y, camZ);
+      shader.use();
+      shader.setVec3("objectColor", 1.0f, 0.0f, 0.0f);
+      shader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+      model = glm::rotate(model, 30.0f, glm::vec3(0, 1, 0));
+      shader.setVec3("lightPos", lightPosition.x, lightPosition.y, lightPosition.z);
+      shader.setVec3("viewPos", viewPosition.x, viewPosition.y, viewPosition.z);
+      shader.setFloat("ambientStrength", ambientStrength);
+      shader.setFloat("diffuseMutiple", diffuseMutiple);
+      shader.setFloat("specularStrength", specularStrength);
+      shader.setFloat("concentrate", concentrate);
+      shader.setMat4("model", model);
+      shader.setMat4("view", view);
+      shader.setMat4("projection", projection);
+    } else if (radioMark == 1) {
+      Gshader.use();
+      Gshader.setVec3("objectColor", 1.0f, 0.0f, 0.0f);
+      Gshader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
+      model = glm::rotate(model, 30.0f, glm::vec3(0, 1, 0));
+      Gshader.setVec3("lightPos", lightPosition.x, lightPosition.y, lightPosition.z);
+      Gshader.setVec3("viewPos", viewPosition.x, viewPosition.y, viewPosition.z);
+      Gshader.setFloat("ambientStrength", ambientStrength);
+      shader.setFloat("diffuseMutiple", diffuseMutiple);
+      Gshader.setFloat("specularStrength", specularStrength);
+      Gshader.setFloat("concentrate", concentrate);
+      Gshader.setMat4("model", model);
+      Gshader.setMat4("view", view);
+      Gshader.setMat4("projection", projection);
+    }
 
-    shader.use();
-    shader.setVec3("objectColor", 1.0f, 0.0f, 0.0f);
-    shader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-
-    shader.use();
-    shader.setVec3("objectColor", 1.0f, 0.0f, 0.0f);
-    shader.setVec3("lightColor", 1.0f, 1.0f, 1.0f);
-
-    glm::mat4 model(1);
-    glm::mat4 view(1);
-    glm::mat4 projection(1);
-    //model = glm::rotate(model, glm::radians(3500.0f), glm::vec3(0.0f, 1.0f, 1.0f));
-    //view = glm::translate(view, glm::vec3(0.0f, 0.0f, -50.0f));
-    view = glm::lookAt(glm::vec3(5.0f, 2.0f, 5.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-    projection = glm::perspective(45.0f, 1.0f, 0.1f, 1000.0f);
-
-    int modelLoc = glGetUniformLocation(shader.ID, "model");
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    int viewLoc = glGetUniformLocation(shader.ID, "view");
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    int proLoc = glGetUniformLocation(shader.ID, "projection");
-    glUniformMatrix4fv(proLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
     glBindVertexArray(cubeVAO);
-    glDrawElements(GL_TRIANGLES, 39, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
     
     lightShader.use();
-    model = glm::mat4(1.0f);
-    model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-    model = glm::translate(model, glm::vec3(1.0f, 1.0f, -1.0f));
-    modelLoc = glGetUniformLocation(lightShader.ID, "model");
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-    viewLoc = glGetUniformLocation(lightShader.ID, "view");
-    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-    proLoc = glGetUniformLocation(lightShader.ID, "projection");
-    glUniformMatrix4fv(proLoc, 1, GL_FALSE, glm::value_ptr(projection));
+    model = glm::mat4();
+    model = glm::scale(model, glm::vec3(0.1f, 0.1f, 0.1f));
+    model = glm::translate(model, lightPosition);
+    lightShader.setMat4("model", model);
+    lightShader.setMat4("view", view);
+    lightShader.setMat4("projection", projection);
     glBindVertexArray(lightVAO);
-    glDrawElements(GL_TRIANGLES, 39, GL_UNSIGNED_INT, 0);
+    glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
-    // check out triggerations & render
-    
+    // render 
     ImGui::Render();
     ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
     glfwSwapBuffers(window);
