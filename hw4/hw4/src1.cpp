7ae42160cc8f1ed1694a0968953c97fd2aa91648
JerryChan31 <331498453@qq.com>
2018-05-20 11:21:08 +0800

    :hw6

--- hw4/hw4/src1.cpp
+++ hw4/hw4/src1.cpp
@@ -7,9 +7,13 @@
 #include "imgui.h"
 #include "imgui/imgui_impl_glfw_gl3.h"
 #include "shader.h"
+#include "camera.h"
 using namespace std;
 #define WINDOW_WIDTH  600
 #define WINDOW_HEIGHT 600
+Camera camera;
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -191,4 +195,12 @@ void processInput(GLFWwindow* window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
     glfwSetWindowShouldClose(window, true);
   }
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    camera.ProcessKeyboard(FORWARD, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    camera.ProcessKeyboard(BACKWARD, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    camera.ProcessKeyboard(LEFT, deltaTime);
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    camera.ProcessKeyboard(RIGHT, deltaTime);
 }
